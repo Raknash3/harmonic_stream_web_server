e6b1ad546259a44c322bb2c0f18270f39c0b5859
Ravi Shankar <ivarraknash3@gmail.com>
2023-12-09 17:58:51 -0500

    first video completed

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const admin = require("../config/firebase.config");
-
+const user = require("../models/user");
 
 router.get("/login", async (req, res) => {
     if (!req.header('authorization')) {
@@ -12,15 +12,43 @@ router.get("/login", async (req, res) => {
     try {
         const decodeValue = await admin.auth().verifyIdToken(token);
         if (!decodeValue) {
-            return res.status(500).json({ message: "Unauthorized" });
+            return res.status(505).json({ message: "Unauthorized" });
         } else {
-            return res.status(200).json(decodeValue);
+            // check if user exists in db
+            const userExists = await user.findOne({ "user_id": decodeValue.user_id });
+            if (!userExists) {
+
+                newUserData(decodeValue, req, res);
+            } else {
+                return res.send("User exists");
+            }
+
         }
-    }
-    catch (error) {
-        return res.status(500).json({ message: error });
+
+    } catch (error) {
+        return res.status(505).json({ message: error });
+
     }
 
 });
+
+const newUserData = async (decodeValue, req, res) => {
+    const newUser = new user({
+        name: decodeValue.name,
+        email: decodeValue.email,
+        imageURL: decodeValue.picture,
+        user_id: decodeValue.user_id,
+        email_verfied: decodeValue.email_verified,
+        role: "member",
+        auth_time: decodeValue.auth_time,
+    })
+
+    try {
+        const savedUser = await newUser.save();
+        res.status(200).send({ user: savedUser });
+    } catch (error) {
+        res.status(400).send({ success: false, msg: error });
+    }
+}
 
 module.exports = router;
